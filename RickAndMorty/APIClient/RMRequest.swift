7e251763794d05fe5936459264cd0eb29d90c8b4
Daniel <labetskiy.02@mail.ru>
2022-12-28 17:49:48 +0300

    Create API Request

--- RickAndMorty/APIClient/RMRequest.swift
+++ RickAndMorty/APIClient/RMRequest.swift
@@ -1,7 +1,54 @@
 import Foundation
 
 final class RMRequest {
+
+    private enum Constants {
+        static let baseUrl = "https://rickandmortyapi.com/api"
+    }
+
+    private let endpoint: RMEndpoint
+
+    private let pathComponents: [String]
+
+    private let queryParametrs: [URLQueryItem]
     
+    /// Constructed url for the api request in string format
+    private var urlString: String {
+        var string = Constants.baseUrl
+        string += "/"
+        string += endpoint.rawValue
+        
+        if !pathComponents.isEmpty {
+            pathComponents.forEach({
+                string += "/\($0)"
+            })
+        }
+        
+        if !queryParametrs.isEmpty {
+            string += "?"
+            
+            let argumentString = queryParametrs.compactMap({
+                guard let value = $0.value else { return nil }
+                return "\($0.name)=\(value)"
+            }).joined(separator: "&")
+            
+            string += argumentString
+        }
+        
+        return string
+    }
     
+    public var url: URL? {
+        return URL(string: urlString)
+    }
     
+    public let httpMrthod = "GET"
+    
+    // MARK: - Public
+
+    init(endpoint: RMEndpoint, pathComponents: [String] = [], queryParametrs: [URLQueryItem] = []) {
+        self.endpoint = endpoint
+        self.pathComponents = pathComponents
+        self.queryParametrs = queryParametrs
+    }
 }
--- RickAndMorty/Controllers/Core/RMCharacterViewController.swift
+++ RickAndMorty/Controllers/Core/RMCharacterViewController.swift
@@ -7,5 +7,15 @@ final class RMCharacterViewController: UIViewController {
         
         view.backgroundColor = .systemBackground
         title = "Characters"
+        
+        let request = RMRequest(endpoint: .character,
+                                queryParametrs: [URLQueryItem(name: "name", value: "rick")
+                                                URLQueryItem(name: "status", value: "alive")
+                                                ])
+        print(request.url)
+        
+        RMService.shared.execute(request, expecting: String.self) { result in
+            
+        }
     }
 }
